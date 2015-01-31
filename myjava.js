34e1b8106107d85d21db01485a8d5ef985cc890f
Steve Rogers <srogers.design@gmail.com>
2015-01-31 12:28:09 -0500

    updated with info about scopes

--- myjava.js
+++ myjava.js
@@ -3,3 +3,44 @@ var color = "red";
 var myDiv = document.getElementById('myDiv');
 myDiv.style.background = "black";
 myDiv.style.color = "#ffffff";
+
+
+var myVar;
+console.log(typeof myVar === "undefined");
+console.log(myVar === undefined);
+// undefined can be overridden
+
+var x = null;
+
+if(x == null){
+  console.log("If");
+} else {
+  console.log("Else");
+}
+
+// == considers null and undefined the same thing
+// === is more strict and will call
+  // back false because they are not the exact same
+
+// Scope
+
+var world = "World!";
+
+function sayHello () {
+  var hello = "Hello";
+
+  function inner () {
+    var extra = " There is more!"
+    console.log(hello + world + extra);
+  }
+  inner()
+}
+
+sayHello();
+
+console.log("world outside sayHello(): ", world);
+
+console.log("hello outside sayHello(): ", hello);
+// you will get an error because hello can only be used
+  //in side the function it is not a global variable like
+  //world is
